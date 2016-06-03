c2530020d1933700ede33eabb5b5ccc2ab3dc4bd
Akash Anand <akash.anand708@gmail.com>
2016-06-03 19:49:21 +0530

    log4j.properties console appender removed.

--- src/main/java/com/birthdaywishes/main/Main.java
+++ src/main/java/com/birthdaywishes/main/Main.java
@@ -2,12 +2,15 @@ package com.birthdaywishes.main;
 
 import java.util.List;
 
+import org.apache.log4j.PropertyConfigurator;
+
 import com.birthdaywishes.mail.Mail;
 import com.birthdaywishes.util.DbUtility;
 
 public class Main {
    public static void main(String[] args) {
-	   
+	   String log4jConfPath = "resources/log4j.properties";
+		PropertyConfigurator.configure(log4jConfPath);
 	   //Get listOfToEmailAddress.
 	   DbUtility dbUtility=new DbUtility();
 	   System.out.println("Fetching Records....");
